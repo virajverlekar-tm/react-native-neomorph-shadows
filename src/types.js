6dee7f304b144caf82f2aae4094f86fc0c62e4b3
hipolitoluc <hipolito.lucas@turtlemint.com>
2025-02-19 12:09:50 +0530

    added deep package for ViewPropTypes

--- src/types.js
+++ src/types.js
@@ -1,44 +1,49 @@
-import { number, shape, string, node, bool } from "prop-types";
+import { number, shape, string, node, bool } from 'prop-types';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 
 export const ShadowARTType = {
-  width: number.isRequired,
-  height: number.isRequired,
-  borderRadius: number,
-  shadowRadius: number,
-  shadowOffset: shape({
-    x: number,
-    y: number,
-  }),
-  shadowOpacity: number,
-  shadowColor: string,
-  backgroundColor: string,
+    width: number.isRequired,
+    height: number.isRequired,
+    borderRadius: number,
+    shadowRadius: number,
+    shadowOffset: shape({
+        x: number,
+        y: number,
+    }),
+    shadowOpacity: number,
+    shadowColor: string,
+    backgroundColor: string,
 };
 
 export const InnerShadowType = {
-  children: node,
-  style: shape({
-    width: number.isRequired,
-    height: number.isRequired,
-  }),
+    ...ViewPropTypes,
+    children: node,
+    style: shape({
+        ...ViewPropTypes.style,
+        width: number.isRequired,
+        height: number.isRequired,
+    }),
 };
 
 export const OuterShadowType = {
-  ...InnerShadowType,
-  inner: bool,
-  useArt: bool,
+    ...InnerShadowType,
+    inner: bool,
+    useArt: bool,
 };
 
 export const NeomorphType = {
-  ...OuterShadowType,
-  darkShadowColor: string,
-  lightShadowColor: string,
-  swapShadows: bool,
+    ...OuterShadowType,
+    darkShadowColor: string,
+    lightShadowColor: string,
+    swapShadows: bool,
 };
 
 export const NeomorphFlexType = {
-  ...NeomorphType,
+    ...NeomorphType,
+    style: ViewPropTypes.style,
 };
 
 export const ShadowFlexType = {
-  ...OuterShadowType,
+    ...OuterShadowType,
+    style: ViewPropTypes.style,
 };
